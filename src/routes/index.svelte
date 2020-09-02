793611cde2b9b7c30473df017cc725dee9ecc1e3
Quinn Stearns <qns@fastmail.com>
2020-09-02 13:18:18 -0700

    Add issue information

---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script>
+    let myDuration = 0
+</script>
+
+
+<svelte:head>
+	<title>The duration seems off.</title>
+</svelte:head>
+
+<p>
+    The duration of this audio is {myDuration}.
+</p>
+
+<audio
+	controls
+	bind:duration={myDuration}
+	on:durationchange={() => console.log("the duration changed")}
+	src="https://media.transistor.fm/4bfbd3c7.mp3" />
